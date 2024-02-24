ea0b0339aa3c84199fd9397a17a6ff39d00dcffc
juliewang <julie_wang2@brown.edu>
2024-02-23 23:05:17 -0500

    initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from "@/components/navigationBar"
+
+export default function Page(){
+    return (
+        <div>
+            <NavigationBar />
+            <p> this is the home page</p>
+
+        </div>
+    );
+}
